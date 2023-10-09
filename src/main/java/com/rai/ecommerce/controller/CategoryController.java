9d020d46cbd5cbbbd144a23af1bd8734aa584f0c
Mausam Rai <mausamrai11@gmail.com>
2023-10-09 12:51:01 -0500

    properties changes for stripe

--- src/main/java/com/rai/ecommerce/controller/CategoryController.java
+++ src/main/java/com/rai/ecommerce/controller/CategoryController.java
@@ -2,7 +2,10 @@ package com.rai.ecommerce.controller;
 
 
 import com.rai.ecommerce.common.ApiResponse;
+import com.rai.ecommerce.exceptions.CustomException;
 import com.rai.ecommerce.model.Category;
+import com.rai.ecommerce.model.User;
+import com.rai.ecommerce.service.AuthenticationService;
 import com.rai.ecommerce.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,13 +22,16 @@ public class CategoryController {
     @Autowired
     CategoryService categoryService;
 
+    @Autowired
+    AuthenticationService authenticationService;
+
     @PostMapping("/create")
     public ResponseEntity<ApiResponse> createCategory(@RequestBody Category category){
         categoryService.createCategory(category);
         return new ResponseEntity<>(new ApiResponse(true, "New Category has been created"), HttpStatus.CREATED);
     }
 
-    @GetMapping("/list")
+    @GetMapping("/")
     public List<Category> listCategory(){
         return categoryService.listCategory();
     }
@@ -39,4 +45,15 @@ public class CategoryController {
         categoryService.editCategory(categoryId, category);
        return new ResponseEntity<>(new ApiResponse(true, "category has been updated"), HttpStatus.OK);
     }
+//    @DeleteMapping("/delete/{categoryId}")
+//    public ResponseEntity<ApiResponse> deleteCartItem(@PathVariable("categoryId") Integer categoryId){
+//        try {
+//            categoryService.deleteCategoryId(categoryId);
+//            return new ResponseEntity<>(new ApiResponse(true, "category has been deleted successfully"), HttpStatus.OK);
+//        }catch ( CustomException e) {
+//            return new ResponseEntity <>( new ApiResponse(true, "Category not found"), HttpStatus.NOT_FOUND);
+//        } catch (Exception e) {
+//           return new ResponseEntity <>( new ApiResponse(true, "An error occurred"), HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//    }
 }
